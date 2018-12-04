c58a40ea4dddbd45309bc7a6af5c2d6274fbe19f
yogesh raheja <yogesh.raheja@thinknyx.com>
2018-12-04 10:22:47 +0000

    Testing file

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "STARTING A SCRIPT FOR THE DEMO"
+touch "/tmp/firstfile"
+touch "/tmp/secondfile"
+echo "TWO FILES HAS BEEN CREATED"
+touch "/tmp/thirdfile"
+touch "/tmp/fourthfile"
+echo "SCRIPT HAS BEEN COMPLETED SUCCESSFULLY"
+
